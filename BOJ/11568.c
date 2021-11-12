acbf3d82c356db0d562ac46aa071da0be3b7ad6e
Jeon Jae Ho <44667299+jaehoo1@users.noreply.github.com>
2021-11-12 22:02:22 +0900

    Create 11568.c

---
+++ BOJ/11568.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main(void) {
+	int N, i, j, max, arr[1000], dp[1000]={1};
+	scanf("%d", &N);
+	for(i=0 ; i<N ; i++)
+		scanf("%d", &arr[i]);
+	for(i=1 ; i<N ; i++) {
+		max=0;
+		for(j=0 ; j<i ; j++)
+			if(dp[j]>max&&arr[i]>arr[j])
+				max=dp[j];
+		dp[i]=max+1;
+	}
+	max=0;
+	for(i=0 ; i<N ; i++)
+		if(dp[i]>max)
+			max=dp[i];
+	printf("%d", max);
+	return 0;
+}
