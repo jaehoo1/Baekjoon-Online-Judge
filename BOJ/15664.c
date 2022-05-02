d4d606a45c0a44dfd8d3ba1e9ca1b479ce122e84
Jeon Jae Ho <jaehoo8936@naver.com>
2022-05-02 22:55:34 +0900

    Create 15664.c

---
+++ BOJ/15664.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int N, M, arr[8], tree[8], visited[8];
+
+int intcmp(const int *a, const int *b) { return *a>*b; }
+void dfs(int depth, int conti) {
+	int i, pre=0;
+	if(depth==M) {
+		for(i=0 ; i<M ; i++)
+			printf("%d ", tree[i]);
+		puts("");
+		return;
+	}
+	for(i=conti ; i<N ; i++)
+		if(!visited[i]&&pre!=arr[i]) {
+			visited[i]=1;
+			tree[depth]=pre=arr[i];
+			dfs(depth+1, i+1);
+			visited[i]=0;
+		}
+}
+
+int main(void) {
+	int i;
+	scanf("%d%d", &N, &M);
+	for(i=0 ; i<N ; i++)
+		scanf("%d", &arr[i]);
+	qsort(arr, N, 4, (int(*)(const void*, const void*))intcmp);
+	dfs(0, 0);
+	return 0;
+}
