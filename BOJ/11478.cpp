a37660015b99899b254c4c5fc01dabe57633bf7a
Jeon Jae Ho <jaehoo8936@naver.com>
2022-05-14 11:22:49 +0900

    Update 11478.cpp

--- BOJ/11478.cpp
+++ BOJ/11478.cpp
@@ -1,16 +1,16 @@
 # include <iostream>
 # include <string>
-# include <unordered_map>
+# include <unordered_set>
 
 using namespace std;
 
 int main(void) {
 	string s;
-	unordered_map<string, bool> um;
+	unordered_set<string> um;
 	cin>>s;
 	for(int i=0 ; i<s.length() ; i++)
 		for(int j=0 ; j<s.length()-i ; j++)
-			um[s.substr(i, j+1)];
+			um.insert(s.substr(i, j+1));
 	cout<<um.size();
 	return 0;
 }
