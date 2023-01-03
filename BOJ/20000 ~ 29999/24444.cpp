4d95dc343dc81f86ebeb2d8fd385479fdb9f1ea1
jaehoo1 <jaehoo8936@naver.com>
2023-01-03 21:06:20 +0900

    24444

---
+++ BOJ/20000 ~ 29999/24444.cpp
@@ -0,0 +1,43 @@
+# include <cstdio>
+# include <vector>
+# include <queue>
+# include <algorithm>
+
+using namespace std;
+
+vector<int> G[100001];
+int visit[100001];
+
+void bfs(int start) {
+	bool visited[100001]={false, };
+	queue<int> q;
+	int cou=1;
+	visited[start]=true;
+	q.push(start);
+	while(!q.empty()) {
+		int cur=q.front();
+		visit[cur]=cou++;
+		q.pop();
+		for(int next : G[cur])
+			if(!visited[next]) {
+				visited[next]=true;
+				q.push(next);
+			}
+	}
+}
+
+int main(void) {
+	int N, M, R, u, v;
+	scanf("%d%d%d", &N, &M, &R);
+	while(M--) {
+		scanf("%d%d", &u, &v);
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+	for(int i=1 ; i<=N ; i++)
+		sort(G[i].begin(), G[i].end());
+	bfs(R);
+	for(int i=1 ; i<=N ; i++)
+		printf("%d\n", visit[i]);
+	return 0;
+}
