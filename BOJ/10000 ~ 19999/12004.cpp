d40bd80f27e9ea16c62416e1033e12bd8e8b118e
jaehoo1 <jaehoo8936@naver.com>
2023-02-19 10:09:33 +0900

    12004

---
+++ BOJ/10000 ~ 19999/12004.cpp
@@ -0,0 +1,57 @@
+# include <cstdio>
+# include <vector>
+# include <unordered_set>
+
+using namespace std;
+
+int parent[3001], size[3001];
+
+int find(int x) {
+	if(x==parent[x])
+		return x;
+	return parent[x]=find(parent[x]);
+}
+void union_set(int a, int b) {
+	a=find(a);
+	b=find(b);
+	if(a==b)
+		return;
+	else if(a<b) {
+		size[a]+=size[b];
+		parent[b]=a;
+	} else {
+		size[b]+=size[a];
+		parent[a]=b;
+	}
+}
+
+int main(void) {
+	int N, M, a, b, close[3000];
+	unordered_set<int> opened;
+	bool connected[3000];
+	vector<int> G[3001];
+	scanf("%d%d", &N, &M);
+	for(int i=1 ; i<=N ; i++) {
+		parent[i]=i;
+		size[i]=1;
+	}
+	for(int i=0 ; i<M ; i++) {
+		scanf("%d%d", &a, &b);
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+	for(int i=0 ; i<N ; i++)
+		scanf("%d", &close[i]);
+	for(int i=N-1 ; i>=0 ; i--) {
+		for(int j=0 ; j<G[close[i]].size() ; j++) {
+			if(opened.find(G[close[i]][j])==opened.end())
+				continue;
+			union_set(close[i], G[close[i]][j]);
+		}
+		opened.insert(close[i]);
+		connected[i]=size[find(close[i])]==N-i?true:false;
+	}
+	for(int i=0 ; i<N ; i++)
+		puts(connected[i]?"YES":"NO");
+	return 0;
+}
