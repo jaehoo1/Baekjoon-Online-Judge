87b36effc67e142293f7ad3261195964b5728277
jaehoo1 <jaehoo8936@naver.com>
2025-07-04 17:10:50 +0900

    32297

---
+++ BOJ/30000 ~ 39999/32297.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        System.out.println(sc.nextLine().contains("gori") ? "YES" : "NO");
+    }
+}
