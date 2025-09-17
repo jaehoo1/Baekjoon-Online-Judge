365805b0e9726b56a6fb1ba3a1a56d0ec8617e2c
jaehoo1 <jaehoo8936@naver.com>
2025-09-17 16:00:30 +0900

    06887

---
+++ BOJ/00000 ~ 09999/06887.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("The largest square has side length " + (int) Math.sqrt(new Scanner(System.in).nextInt()) + ".");
+    }
+}
