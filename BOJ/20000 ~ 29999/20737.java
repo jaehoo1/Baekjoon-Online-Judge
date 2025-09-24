999fe9deb0ed2f3f389552cfc70d01ec861788e0
jaehoo1 <jaehoo8936@naver.com>
2025-09-24 12:59:31 +0900

    20737

---
+++ BOJ/20000 ~ 29999/20737.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%.7f", new Scanner(System.in).nextDouble() - 1);
+    }
+}
