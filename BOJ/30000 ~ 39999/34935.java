5d5fb336e43c655ec5045e764cfde4e66dd988db
jaehoo1 <jaehoo8936@naver.com>
2025-12-23 19:50:36 +0900

    34935

---
+++ BOJ/30000 ~ 39999/34935.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] A = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Long.parseLong(st.nextToken());
+            if (i > 0 && A[i] <= A[i - 1]) {
+                System.out.println(0);
+                System.exit(0);
+            }
+        }
+        System.out.println(1);
+    }
+}
