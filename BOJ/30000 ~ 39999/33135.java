35f9512fec25c352be522f9b16c5d8a65a8bce3e
jaehoo1 <jaehoo8936@naver.com>
2025-12-26 12:30:22 +0900

    33135

---
+++ BOJ/30000 ~ 39999/33135.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+        System.out.println(s.length() - set.size());
+    }
+}
