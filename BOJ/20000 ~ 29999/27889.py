24925574bd4658718e2a3e80b02057ef0bebbc22
jaehoo1 <jaehoo8936@naver.com>
2026-01-01 20:26:35 +0900

    27889

---
+++ BOJ/20000 ~ 29999/27889.py
@@ -0,0 +1,9 @@
+school = input()
+if school == "NLCS":
+    print("North London Collegiate School")
+elif school == "BHA":
+    print("Branksome Hall Asia")
+elif school == "KIS":
+    print("Korea International School")
+elif school == "SJA":
+    print("St. Johnsbury Academy")
