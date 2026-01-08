d3736a3e208925f2f2c48ba8f1d0564315c7cfc6
jaehoo1 <jaehoo8936@naver.com>
2026-01-08 20:33:48 +0900

    34813

---
+++ BOJ/30000 ~ 39999/34813.py
@@ -0,0 +1,10 @@
+char = input()[0]
+if char == 'F':
+    subject = "Foundation"
+elif char == 'C':
+    subject = "Claves"
+elif char == 'V':
+    subject = "Veritas"
+elif char == 'E':
+    subject = "Exploration"
+print(subject)
