4e950d942a7834e64f1c424c617439e3a8659ba9
jaehoo1 <jaehoo8936@naver.com>
2026-01-13 09:10:35 +0900

    32800

---
+++ BOJ/30000 ~ 39999/32800.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int busPeople = 0;
+        int max = 0;
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            busPeople += -a + b;
+            max = Math.max(max, busPeople);
+        }
+        System.out.println(max);
+    }
+}
