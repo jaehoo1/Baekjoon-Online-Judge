4832dd88eec30bf1f403ed41980cd6516d66e912
jaehoo1 <jaehoo8936@naver.com>
2026-01-28 12:53:25 +0900

    22061

---
+++ BOJ/20000 ~ 29999/22061.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+            bw.write((c - 2 * Math.min(c / 2, b) <= a) ? "YES\n" : "NO\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
