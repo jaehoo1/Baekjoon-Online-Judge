7d06bb9bd71493f7456e9185fce537e891fa3901
jaehoo1 <jaehoo8936@naver.com>
2026-02-17 16:41:43 +0900

    06825

---
+++ BOJ/00000 ~ 09999/06825.py
@@ -0,0 +1,9 @@
+weight = float(input())
+height = float(input())
+bmi = weight / (height * height)
+if bmi > 25:
+    print("Overweight")
+elif bmi >=18.5:
+    print("Normal weight")
+else:
+    print("Underweight")
